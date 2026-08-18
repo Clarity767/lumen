81d408cbc1250858e44478e4478a839f7e79e6c0
Глеб Шелег <shelehgleb@gmail.com>
2026-08-18 19:18:41 +0300

    fix

--- Lumen/build.sh
+++ Lumen/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
+python manage.py findstatic restaurant/css/base.css --verbosity 3
 python manage.py collectstatic --no-input
 python manage.py migrate
-# python manage.py loaddata flights_data.json
